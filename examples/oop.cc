baf9b9e3e1ccb5424715398349ac41b080d93005
xcrtp <illuminatestar@foxmail.com>
2026-02-13 08:39:34 +0800

    init

---
+++ examples/oop.cc
@@ -0,0 +1,48 @@
+#include <iostream>
+
+#include "xcmixin/xcmixin.hpp"
+
+XCMIXIN_METHOD_DEF_BEGIN(dosomethings1_method)
+void dosomethings1() { std::cout << "dosomethings1" << std::endl; }
+XCMIXIN_METHOD_DEF_END()
+XCMIXIN_METHOD_DEF_BEGIN(name_method)
+std::string name() { return "Unknown"; }
+XCMIXIN_METHOD_DEF_END()
+
+XCMIXIN_METHOD_DEF_BEGIN(new_name_method)
+std::string name() { return "NewName"; }
+XCMIXIN_METHOD_DEF_END()
+
+XCMIXIN_METHOD_DEF_BEGIN(print_method)
+void print() {
+    xcmixin_require_method(name_method);
+    std::cout << "Hello, " << xcmixin_self.name() << std::endl;
+}
+XCMIXIN_METHOD_DEF_END()
+template <typename Base>
+class MyClass;
+
+using recorder = xcmixin::method_recorder<dosomethings1_method, print_method,
+                                          name_method, new_name_method>;
+template <typename Base>
+class MyClass
+    : public xcmixin::impl_methods_recorders<MyClass<Base>, recorder> {
+    static_assert(
+        xcmixin::impl_methods_recorders<MyClass<Base>, recorder>::valid_class(),
+        "MyClass must be valid");
+};
+
+XCMIXIN_IMPL_METHOD_BEGIN(name_method, typename B)
+XCMIXIN_IMPL_METHOD_FOR(MyClass<B>)
+std::string name() { return "MyClass"; }
+XCMIXIN_METHOD_REQUIRES(xcmixin_no_shadow(name);)
+XCMIXIN_IMPL_METHOD_END()
+
+int main() {
+    MyClass<void> my_class;
+
+    my_class.dosomethings1();
+    my_class.name();
+    my_class.print();
+    return 0;
+}
