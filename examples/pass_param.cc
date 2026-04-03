c86b3b8392bf961e96243871981fb06d4b09957b
xcrtp <xcrtplt@outlook.com>
2026-04-03 11:03:25 +0800

    feat: add XCMIXIN_CONSTRUCTOR macro for forwarding parameters to base mixin
    - Add XCMIXIN_CONSTRUCTOR macro that generates constructors forwarding
      all parameters to the base class in the mixin inheritance chain
    - Update pass_param.cc example to demonstrate usage

--- examples/pass_param.cc
+++ examples/pass_param.cc
@@ -1,10 +1,30 @@
 #include <iostream>
 #include <string>
+#include <type_traits>
 #include <xcmixin/xcmixin.hpp>
 
-XCMIXIN_DEF_BEGIN(logable)
-// logable(std::string name_scope) : name_scope_(std::move(name_scope)) {}
+XCMIXIN_DEF_BEGIN(logable2)
+XCMIXIN_CONSTRUCTOR(logable2, std::string name_scope1, std::string name_scope2)
+, name_scope1_(std::move(name_scope1)), name_scope2_(std::move(name_scope2)) {}
 void log(std::string_view msg) const {
+    std::cout << "log from " << name_scope1_ << " msg: " << msg << std::endl;
+    std::cout << "log from " << name_scope2_ << " msg: " << msg << std::endl;
+}
+
+private:
+std::string name_scope1_{};
+std::string name_scope2_{};
+XCMIXIN_DEF_END()
+XCMIXIN_DEF_BEGIN(logable)
+XCMIXIN_CONSTRUCTOR(logable, std::string name_scope)
+, name_scope_(std::move(name_scope)) {}
+template <typename = void>
+void log(std::string_view msg) const {
+    std::cout << "log from " << name_scope_ << " msg: " << msg << std::endl;
+}
+template <>
+auto log(std::string_view msg) const -> std::void_t<decltype(&Base::log)> {
+    Base::log(msg);
     std::cout << "log from " << name_scope_ << " msg: " << msg << std::endl;
 }
 
@@ -12,17 +32,17 @@ private:
 std::string name_scope_{};
 XCMIXIN_DEF_END()
 
-class MyClass : public xcmixin::impl_mixin<MyClass, logable> {
+class MyClass : public xcmixin::impl_mixin<MyClass, logable, logable2> {
    public:
-    MyClass(std::string name_scope) : xcmixin_self_class(
-        
-
-    ) {}
+    template <typename... Args>
+    MyClass(Args&&... args) : xcmixin_self_class(std::forward<Args>(args)...) {}
     xcmixin_init_class;
 };
 
 int main() {
-    MyClass my_class{"my_class"};
-    my_class.log("hello world");
+    MyClass c("my_class", "logable2", "logable");
+
+    c.log("hello world");
+
     return 0;
 }
